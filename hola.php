a33299e9f199d701c26c7f6a76cc2347b50cc480
unknown <cristian_15_12_3@hotmail.com>
2017-12-11 11:15:08 -0500

    primero commit

---
+++ hola.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Introducing Lollipop, a sweet new take on Android.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>123Android</title>
+
+  <!-- <meta http-equiv="Refresh" content="3;url=login.html"> -->
+   
+  <body>
+	pruebaa.....  	
+    pruebaa.....    
+      pruebaa.....    
+        pruebaa.....    
+          pruebaa.....    
+            pruebaa.....    
+            v
+            
+  </body>
+
+</html>
